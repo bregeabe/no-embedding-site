adc7f3e84af3bfb0dc6862b6762a20f8362123e9
bregeabe <abrege11@gmail.com>
2026-04-09 21:26:43 -0400

    institutions placeholder

---
+++ src/components/screens/institutions/institutions.tsx
@@ -0,0 +1,7 @@
+export const institutions = [
+  { name: 'MIT', logo: '' },
+  { name: 'Oxford', logo: '' },
+  { name: 'Caltech', logo: '' },
+  { name: 'ETH Zürich', logo: '' },
+  { name: 'Waterloo', logo: '' },
+]
