aafc893a66c75797e869293fbfdda6ddc5bdc8df
Abe Brege <bregeabe@gmail.com>
2026-04-09 21:24:32 -0400

    literature placeholder

---
+++ src/components/screens/literature/literature.jsx
@@ -0,0 +1,7 @@
+export const papers = [
+  { id: 1, src: '', alt: 'Paper 1' },
+  { id: 2, src: '', alt: 'Paper 2' },
+  { id: 3, src: '', alt: 'Paper 3' },
+  { id: 4, src: '', alt: 'Paper 4' },
+  { id: 5, src: '', alt: 'Paper 5' },
+]
